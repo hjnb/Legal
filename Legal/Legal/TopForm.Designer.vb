586fa54e7fd27195a01d68a3c00c8c93c94074c0
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-06-26 17:09:01 +0900

    とりあえずデザイン作成等。

---
+++ Legal/Legal/TopForm.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TopForm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.yyBox = New System.Windows.Forms.ComboBox()
+        Me.dgvHol = New System.Windows.Forms.DataGridView()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.dateBox = New ymdBox.ymdBox()
+        Me.HolBox = New System.Windows.Forms.TextBox()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnDeleteYear = New System.Windows.Forms.Button()
+        Me.btnCreateYear = New System.Windows.Forms.Button()
+        Me.PicBox = New System.Windows.Forms.PictureBox()
+        CType(Me.dgvHol, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'yyBox
+        '
+        Me.yyBox.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.yyBox.FormattingEnabled = True
+        Me.yyBox.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.yyBox.Location = New System.Drawing.Point(21, 17)
+        Me.yyBox.MaxLength = 4
+        Me.yyBox.Name = "yyBox"
+        Me.yyBox.Size = New System.Drawing.Size(64, 23)
+        Me.yyBox.TabIndex = 0
+        '
+        'dgvHol
+        '
+        Me.dgvHol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvHol.Location = New System.Drawing.Point(102, 17)
+        Me.dgvHol.Name = "dgvHol"
+        Me.dgvHol.RowTemplate.Height = 21
+        Me.dgvHol.Size = New System.Drawing.Size(180, 430)
+        Me.dgvHol.TabIndex = 1
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(301, 23)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(58, 21)
+        Me.btnPrint.TabIndex = 2
+        Me.btnPrint.Text = "ﾌﾟﾘﾝﾄ"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'dateBox
+        '
+        Me.dateBox.boxType = 4
+        Me.dateBox.DateText = ""
+        Me.dateBox.EraLabelText = "R01"
+        Me.dateBox.EraText = ""
+        Me.dateBox.Location = New System.Drawing.Point(301, 214)
+        Me.dateBox.MonthLabelText = "06"
+        Me.dateBox.MonthText = ""
+        Me.dateBox.Name = "dateBox"
+        Me.dateBox.Size = New System.Drawing.Size(145, 34)
+        Me.dateBox.TabIndex = 3
+        Me.dateBox.textReadOnly = False
+        '
+        'HolBox
+        '
+        Me.HolBox.Location = New System.Drawing.Point(302, 264)
+        Me.HolBox.Name = "HolBox"
+        Me.HolBox.Size = New System.Drawing.Size(118, 19)
+        Me.HolBox.TabIndex = 4
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(304, 300)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(58, 24)
+        Me.btnRegist.TabIndex = 5
+        Me.btnRegist.Text = "行登録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(361, 300)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(58, 24)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.Text = "行削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnDeleteYear
+        '
+        Me.btnDeleteYear.Location = New System.Drawing.Point(361, 323)
+        Me.btnDeleteYear.Name = "btnDeleteYear"
+        Me.btnDeleteYear.Size = New System.Drawing.Size(58, 24)
+        Me.btnDeleteYear.TabIndex = 8
+        Me.btnDeleteYear.Text = "年抹消"
+        Me.btnDeleteYear.UseVisualStyleBackColor = True
+        '
+        'btnCreateYear
+        '
+        Me.btnCreateYear.Location = New System.Drawing.Point(304, 323)
+        Me.btnCreateYear.Name = "btnCreateYear"
+        Me.btnCreateYear.Size = New System.Drawing.Size(58, 24)
+        Me.btnCreateYear.TabIndex = 7
+        Me.btnCreateYear.Text = "年生成"
+        Me.btnCreateYear.UseVisualStyleBackColor = True
+        '
+        'PicBox
+        '
+        Me.PicBox.Location = New System.Drawing.Point(304, 87)
+        Me.PicBox.Name = "PicBox"
+        Me.PicBox.Size = New System.Drawing.Size(115, 92)
+        Me.PicBox.TabIndex = 9
+        Me.PicBox.TabStop = False
+        '
+        'TopForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 459)
+        Me.Controls.Add(Me.PicBox)
+        Me.Controls.Add(Me.btnDeleteYear)
+        Me.Controls.Add(Me.btnCreateYear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnRegist)
+        Me.Controls.Add(Me.HolBox)
+        Me.Controls.Add(Me.dateBox)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.dgvHol)
+        Me.Controls.Add(Me.yyBox)
+        Me.Name = "TopForm"
+        Me.Text = "祝祭日"
+        CType(Me.dgvHol, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents yyBox As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvHol As System.Windows.Forms.DataGridView
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents dateBox As ymdBox.ymdBox
+    Friend WithEvents HolBox As System.Windows.Forms.TextBox
+    Friend WithEvents btnRegist As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnDeleteYear As System.Windows.Forms.Button
+    Friend WithEvents btnCreateYear As System.Windows.Forms.Button
+    Friend WithEvents PicBox As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Legal/Legal/TopForm.vb
@@ -0,0 +1,146 @@
+﻿Public Class TopForm
+
+    'データベースのパス
+    Public dbFilePath As String = My.Application.Info.DirectoryPath & "\Legal.mdb"
+    Public DB_Legal As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbFilePath
+
+    'エクセルのパス
+    Public excelFilePass As String = My.Application.Info.DirectoryPath & "\Legal.xls"
+
+    '画像パス
+    Public topImageFilePath As String = My.Application.Info.DirectoryPath & "\Legal.PNG"
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub New()
+        InitializeComponent()
+
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub TopForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'データベース、エクセル、構成ファイルの存在チェック
+        If Not System.IO.File.Exists(dbFilePath) Then
+            MsgBox("データベースファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        'If Not System.IO.File.Exists(excelFilePass) Then
+        '    MsgBox("エクセルファイルが存在しません。ファイルを配置して下さい。")
+        '    Me.Close()
+        '    Exit Sub
+        'End If
+
+        If Not System.IO.File.Exists(topImageFilePath) Then
+            MsgBox("トップ画像ファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        '画像の配置処理
+        PicBox.ImageLocation = topImageFilePath
+
+        '西暦ボックスの初期設定
+        initYYBox()
+
+        'データグリッドビュー初期設定
+        initDgvHol()
+    End Sub
+
+    ''' <summary>
+    ''' 画像クリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub PicBox_Click(sender As System.Object, e As System.EventArgs) Handles PicBox.Click
+        Me.Close()
+    End Sub
+
+    ''' <summary>
+    ''' 西暦ボックス初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initYYBox()
+        'クリア
+        yyBox.Items.Clear()
+
+        'データ取得
+        Dim cn As New ADODB.Connection()
+        cn.Open(DB_Legal)
+        Dim sql As String = "select distinct YY from Hol order by YY Desc"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        While Not rs.EOF
+            Dim yy As String = Util.checkDBNullValue(rs.Fields("YY").Value)
+            yyBox.Items.Add(yy)
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cn.Close()
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvHol()
+        Util.EnableDoubleBuffering(dgvHol)
+
+        With dgvHol
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .DefaultCellStyle.BackColor = Color.FromKnownColor(KnownColor.Control)
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .RowHeadersVisible = False
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ColumnHeadersVisible = False
+            .RowTemplate.Height = 18
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .ReadOnly = True
+            '.Font = New Font("ＭＳ Ｐゴシック", 9)
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' データ表示
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub displayDgvHol(year As String)
+        'クリア
+        dgvHol.Columns.Clear()
+
+
+
+
+
+    End Sub
+
+    ''' <summary>
+    ''' 西暦ボックス値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub yyBox_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles yyBox.SelectedIndexChanged
+        displayDgvHol(yyBox.Text)
+    End Sub
+End Class
